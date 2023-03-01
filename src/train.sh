e723f514f2e03d57eba501c07b1b142753729131
Caetano Mazzoni Ranieri <cmranieri2@gmail.com>
2023-03-01 18:22:40 -0300

    confidence analysis

--- src/train.sh
+++ src/train.sh
@@ -1,42 +1,42 @@
 #!/usr/bin/bash
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/flow_config.yaml 
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/single_rgb_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/single_rgb_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/single_rgb_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/single_rgb_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/enoe_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/enoe_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/enoe_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/enoe_config.yaml 
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/single_gray_flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/single_gray_flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/single_gray_flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/single_gray_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/gray_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/gray_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/gray_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/gray_flow_config.yaml 
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/stack_flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/stack_flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/stack_flow_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/stack_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/stack_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/stack_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/stack_flow_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/stack_flow_config.yaml 
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/single_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/single_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/single_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/single_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/flow_diffs_config.yaml 
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/single_gray_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/single_gray_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/single_gray_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/single_gray_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/gray_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/gray_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/gray_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/gray_flow_diffs_config.yaml 
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/stack_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/stack_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/stack_flow_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/stack_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/stack_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/stack_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/stack_flow_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/stack_flow_diffs_config.yaml 
 
-python /workspace/src/main.py --eval_only true --split 1 --config_path ../configs/pair_gray_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 2 --config_path ../configs/pair_gray_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 3 --config_path ../configs/pair_gray_diffs_config.yaml 
-python /workspace/src/main.py --eval_only true --split 4 --config_path ../configs/pair_gray_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 1 --config_path ../configs/pair_gray_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 2 --config_path ../configs/pair_gray_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 3 --config_path ../configs/pair_gray_diffs_config.yaml 
+python /workspace/src/main.py --eval_only false --split 4 --config_path ../configs/pair_gray_diffs_config.yaml 
 
