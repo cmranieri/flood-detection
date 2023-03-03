d2e4740fbbd248d9a23dfee08830eb92aa39af2a
Caetano Mazzoni Ranieri <cmranieri2@gmail.com>
2023-03-03 15:47:18 -0300

    new evaluations

--- src/train.sh
+++ src/train.sh
@@ -44,12 +44,20 @@
 #python /workspace/src/main.py --split 2 --config_path ../configs/rgbdiff_config.yaml 
 #python /workspace/src/main.py --split 3 --config_path ../configs/rgbdiff_config.yaml 
 #python /workspace/src/main.py --split 4 --config_path ../configs/rgbdiff_config.yaml 
+#
+#python /workspace/src/main.py --split 1 --config_path ../configs/rgbdiff_diffs_config.yaml 
+#python /workspace/src/main.py --split 2 --config_path ../configs/rgbdiff_diffs_config.yaml 
+#python /workspace/src/main.py --split 3 --config_path ../configs/rgbdiff_diffs_config.yaml 
+#python /workspace/src/main.py --split 4 --config_path ../configs/rgbdiff_diffs_config.yaml 
 
-python /workspace/src/main.py --split 1 --config_path ../configs/rgbdiff_diffs_config.yaml 
-python /workspace/src/main.py --split 2 --config_path ../configs/rgbdiff_diffs_config.yaml 
-python /workspace/src/main.py --split 3 --config_path ../configs/rgbdiff_diffs_config.yaml 
-python /workspace/src/main.py --split 4 --config_path ../configs/rgbdiff_diffs_config.yaml 
+python /workspace/src/main.py --split 1 --config_path ../configs/pair_gray_config.yaml 
+python /workspace/src/main.py --split 2 --config_path ../configs/pair_gray_config.yaml 
+python /workspace/src/main.py --split 3 --config_path ../configs/pair_gray_config.yaml 
+python /workspace/src/main.py --split 4 --config_path ../configs/pair_gray_config.yaml 
 
-
+python /workspace/src/main.py --split 1 --config_path ../configs/rgb_diffs_config.yaml 
+python /workspace/src/main.py --split 2 --config_path ../configs/rgb_diffs_config.yaml 
+python /workspace/src/main.py --split 3 --config_path ../configs/rgb_diffs_config.yaml 
+python /workspace/src/main.py --split 4 --config_path ../configs/rgb_diffs_config.yaml 
 
 
